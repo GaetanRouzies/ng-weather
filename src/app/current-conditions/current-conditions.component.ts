b88276fe5702605f55e139696ed56cfdcc4e2cee
Gaëtan Rouziès <rouzies.gaetan@gmail.com>
2024-06-21 10:07:15 +0200

    Renaming variables

--- src/app/current-conditions/current-conditions.component.ts
+++ src/app/current-conditions/current-conditions.component.ts
@@ -11,8 +11,8 @@ import {ConditionsAndZip} from '../conditions-and-zip.type';
 })
 export class CurrentConditionsComponent {
 
-    private weatherService = inject(WeatherService);
     private router = inject(Router);
+    protected weatherService = inject(WeatherService);
     protected locationService = inject(LocationService);
     protected currentConditionsByZip: Signal<ConditionsAndZip[]> = this.weatherService.getCurrentConditions();
 
